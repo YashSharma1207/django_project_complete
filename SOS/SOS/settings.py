6475dc8c34567f1008285834a679832f386bac2e
Yash122003 <rajeshkanha16@gmail.com>
2025-01-03 16:13:01 +0530

    03 jan 2025

--- SOS/SOS/settings.py
+++ SOS/SOS/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yashsharma192020@gmail.com'
-EMAIL_HOST_PASSWORD = 'zmsy yqbv vrvn eiex'
+EMAIL_HOST_USER = 'xyz@gmail.com'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
